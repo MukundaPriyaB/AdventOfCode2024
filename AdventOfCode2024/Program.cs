2a4e1623517da1fe9a7323794309c40a1777c1e7
Priya <Priya@MP-LENOVO-S340>
2025-10-04 10:10:12 +0100

    Add project files.

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Solutions
+{
+    static void Main(string[] args)
+    {
+        List<int> leftSideList = new List<int>();
+        List<int> rightSideList = new List<int>();
+
+        int i1;
+        int i2;
+        int numberoflines = 0;
+
+        //string filePath = "C:\\workspace\\dotNetProjects\\AdevntOfCode2024InputTexts\\Day1InputSample.txt";
+        string filePath = "C:\\workspace\\dotNetProjects\\AdevntOfCode2024InputTexts\\Day1Part1.txt";
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] partsWithoutEmpty = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                i1 = Convert.ToInt32(partsWithoutEmpty[0]);
+                i2 = Convert.ToInt32(partsWithoutEmpty[1]);
+                leftSideList.Add(i1);
+                rightSideList.Add(i2);
+                numberoflines++;
+            }
+        }
+        Console.WriteLine(numberoflines);
+        leftSideList.Sort();
+        rightSideList.Sort();
+        int differenceOfValues = 0;
+        int sumOfTheDifferences = 0;
+        for (int i = 0; i < leftSideList.Count; i++)
+        {
+            differenceOfValues = rightSideList[i] - leftSideList[i];
+
+            if (differenceOfValues < 0)
+            {
+                differenceOfValues = -differenceOfValues;
+            }
+            sumOfTheDifferences += differenceOfValues;
+        }
+
+        Console.WriteLine(sumOfTheDifferences);
+    }
+}
